4013fb58d94b9a3b3350cde43862b9fa460ad4c4
James Jarvis <jjhaslanded@gmail.com>
2019-01-12 18:41:29 -0800

    slight modifications

--- convert-to-bash.js
+++ convert-to-bash.js
@@ -34,15 +34,15 @@ function getStartDate() {
 Returns a string of commit information for that day, based on the number of commits requested.
 */
 function commit(date, commits) {
-  let commitDate = date;
+  let commitDate = new Date(date.valueOf());
   let dayCommitInstructions = [];
   for (i = 0; i < commits; i++) {
     dayCommitInstructions.push(
       `GIT_AUTHOR_DATE=${commitDate.toUTCString()} GIT_COMMITTER_DATE=${commitDate.toUTCString()}
-      git commit --allow-empty -m "git-art" > /dev/null`);
-      commitDate.setUTCMinutes(commitDate.getUTCMinutes() + 1);
+      git commit --allow-empty -m "git-art" > /dev/null`
+    );
+    commitDate.setUTCMinutes(commitDate.getUTCMinutes() + 1);
   }
-  // console.log(commitInstructions.join('\n'));
   return dayCommitInstructions;
 }
 
@@ -50,21 +50,24 @@ function commit(date, commits) {
 This function should convert the supplied 2d array from the parameter into a bash script which can be run by the user.
 The bash script should take the 2d array, create a git repo with the required commits and then upload it to the repo specified.
 */
-function generateBash(image_array, multiplier=1) {
+function generateBash(image_array, multiplier = 1) {
   let startDate = getStartDate();
   const length = Math.min(...image_array.map(x => x.length));
   let commitInstructions = [];
+
   for (week = 0; week < length; week++) {
     for (day = 0; day < image_array.length; day++) {
-      const daysCommits = commit(startDate, image_array[day][week] * multiplier);
+      const daysCommits = commit(
+        startDate,
+        image_array[day][week] * multiplier
+      );
       if (daysCommits.length > 0) {
-        commitInstructions.push(daysCommits.join('\n'));
+        commitInstructions.push(daysCommits.join("\n"));
       }
-      // console.log(image_array[day][week]);
       startDate.setDate(startDate.getDate() + 1);
     }
   }
-  return commitInstructions.join('\n');
+  return commitInstructions.join("\n");
 }
 
 console.log(generateBash(TEMPLATE_ARRAY));
