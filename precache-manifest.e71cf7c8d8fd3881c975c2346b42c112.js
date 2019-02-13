b7d363879231bdbad2e639a3b539bf52c540751a
jamesjarvis-io-deployer <jameshfj98@yahoo.co.uk>
2019-02-13 07:28:42 +0000

    Automated deployment to GitHub Pages: 2ce864be4a270dfda95383b357498d5f654a71d6

---
+++ precache-manifest.e71cf7c8d8fd3881c975c2346b42c112.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf940cbcb0f8c99e6d8b",
+    "url": "/git-art/static/js/runtime~main.cf940cbc.js"
+  },
+  {
+    "revision": "c745ac7b5c07c78fb313",
+    "url": "/git-art/static/js/main.c745ac7b.chunk.js"
+  },
+  {
+    "revision": "8af9f936eecbdb60f9fd",
+    "url": "/git-art/static/js/1.8af9f936.chunk.js"
+  },
+  {
+    "revision": "c745ac7b5c07c78fb313",
+    "url": "/git-art/static/css/main.aa5ab711.chunk.css"
+  },
+  {
+    "revision": "38d32c7d57d7991b6f400b5b6ca34ee1",
+    "url": "/git-art/index.html"
+  }
+];
